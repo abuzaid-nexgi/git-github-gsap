e3896c4bb897bac63e40820f6d35e56227cf812d
Abu Zaid <127389591+Abuzaid98@users.noreply.github.com>
2024-01-08 12:28:38 +0530

    modify animation

--- index.js
+++ index.js
@@ -10,35 +10,36 @@ gsap.to(sections, {
   scrollTrigger: {
     trigger: ".combine",
     pin: true,
-    scrub: 1,
+    scrub: true,
     end: () => `+=${document.querySelector(".combine").offsetWidth}`
   }
 });
 
 gsap.fromTo('.square', {
-    opacity: 0
-  }, {
-    opacity: 1,
-    ease: 'ease',
-    scrollTrigger: {
-      trigger: '.parallax4',
-      start: 'top bottom',
-      end: 'top center',
-      scrub: 1
-    }
-  });
+  opacity: 0,
+  y:0,
+}, {
+  opacity: 1,
+  ease: 'ease',
+  y:200,
+  scrollTrigger: {
+    trigger: '.parallax4',
+    start: 'top bottom',
+    end: 'top center',
+    scrub: true,
+  }
+});
 
-gsap.fromTo('.square2', {
-    translateY: 0,
-    opacity:0
-  }, {
-    translateY: -1000,
-    opacity:1,
-    ease: 'ease',
-    scrollTrigger: {
-      trigger: '.parallax4',
-      start: 'top bottom',
-      end: 'top center',
-      scrub: 1,
-    }
-  });
+gsap.to('.square2', {
+  scrollTrigger: {
+    trigger: '.square2',
+    // start:'top bottom',
+    // toggleActions: 'restart none reverse none',
+    scrub:true,
+  },
+  x: -400,
+  y: -400,
+  rotate: 360,
+  duration: 3,
+
+});
